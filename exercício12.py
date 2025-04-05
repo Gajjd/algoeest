831cd955e76fdabdc2730702129b494f6b6e9bc6
Gabriel Antonio Andrighetto <107733745+Gajjd@users.noreply.github.com>
2025-04-05 13:16:59 -0400

    Update exercício12.py

--- exercício12.py
+++ exercício12.py
@@ -1,7 +1,12 @@
-acesso = True
 saldo = int(input("Insira o saldo dessa conta."))
+
+acesso = True
+
 if saldo >0 :
     print("Conta ativa.")
 else :
     acesso = False
     print("Conta desativada.")
+
+#   # Facilitar para outros programas
+# print(f"acesso: {acesso}")
