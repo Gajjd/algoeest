07515c2dfbd289c4e023a14ca59d13df583dbea3
Gabriel Antonio Andrighetto <107733745+Gajjd@users.noreply.github.com>
2025-04-05 12:47:09 -0400

    Update exercício13.py

--- exercício13.py
+++ exercício13.py
@@ -1,9 +1,14 @@
 nome = input("Nome: ")
 idade = int(input("Idade: "))
 turma = input("Turma: ")
+
 matricula = True
+
 if idade >= 6:
     print(f"Aluno cadastrado com sucesso: {nome}, {idade} anos, turma {turma}")
 else:
     matricula = False
     print("Matrícula negada")
+
+#   # Facilitar para outros programas
+# print(f"Matrícula: {matricula}")
