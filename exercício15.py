8e7c50fb9ab4460a4e3544a0eaf9efdc6de3e1ed
Gabriel Antonio Andrighetto <107733745+Gajjd@users.noreply.github.com>
2025-04-05 12:37:06 -0400

    Update exercício15.py

--- exercício15.py
+++ exercício15.py
@@ -1,8 +1,10 @@
 nome_produto = input("Nome do produto: ")
 quantidade = int(input("Quantidade: "))
 preco = float(input("Preço: "))
+
 valor_total = quantidade * preco
 desconto5 = valor_total - valor_total*0.05
+
 if valor_total <=100:
     print(f"Total: R${valor_total}")
 else:
