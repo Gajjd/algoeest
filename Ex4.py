9764bd241ffafe05c7c7d7b91aa1435edadae79d
Gabriel Antonio Andrighetto <107733745+Gajjd@users.noreply.github.com>
2025-04-05 13:34:11 -0400

    Update Ex4.py

--- Ex4.py
+++ Ex4.py
@@ -1,5 +1,7 @@
-num= int(input("Insira um número"))
-if num%2==0:
+num = int(input("Insira um número"))
+
+if num % 2 == 0:
     print(f"O número {num} é par")
+    
 else:
     print(f"O número {num} é ímpar")
