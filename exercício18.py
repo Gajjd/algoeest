4e9786137b13bb2f91db43468ad0210d2e0ee258
Gabriel Antonio Andrighetto <107733745+Gajjd@users.noreply.github.com>
2025-04-05 12:18:32 -0400

    Update exercício18.py

--- exercício18.py
+++ exercício18.py
@@ -1,5 +1,6 @@
 salario = float(input("Salário fixo: "))
 horas_extra = float(input("Horas extras: "))
 valor_hora_extra = float(input("Valor por horas extras: "))
+
 salario_total = salario + horas_extra*valor_hora_extra
 print(f"Salário: R${salario_total}")
