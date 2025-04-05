d0518642ac2acb6d80478d970039429b20876a43
Gabriel Antonio Andrighetto <107733745+Gajjd@users.noreply.github.com>
2025-04-05 13:29:48 -0400

    Update exercício10.py

--- exercício10.py
+++ exercício10.py
@@ -1,12 +1,17 @@
-valor = int(input("Insira o valor da compra"))
+valor = float(input("Insira o valor da compra"))
+
 descont10 = valor-valor*0.1
 descont20 = valor-valor*0.2
 descont25 = valor-valor*0.25
+
 if valor<100:
-    print(valor)
+    print(f"Total: R${valor}")
+
 elif valor >=100 and valor <500:
-    print(descont10)
+    print(f"Total: R${descont10}")
+
 elif valor >=500 and valor<1000:
-    print(descont20)
+    print(f"Total: R${descont20}")
+
 else:
-    print(descont25)
+    print(f"Total: R${descont25}")
