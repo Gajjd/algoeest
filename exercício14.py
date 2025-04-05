59e787f29e6cabc93108dd75530dddc06ecd8a02
Gabriel Antonio Andrighetto <107733745+Gajjd@users.noreply.github.com>
2025-04-05 12:43:25 -0400

    Update exercício14.py

--- exercício14.py
+++ exercício14.py
@@ -1,8 +1,13 @@
 usuario = input("Insira o seu nome de usuário: ")
 senha = input("Insira sua senha: ")
-acesso = True
+
+acesso = False
+
 if usuario == "admin" and senha == "1234":
+    acesso = True
     print(f"Bem vindo, {usuario}")
 else:
-    acesso = False
     print(f"Usuário ou senha incorretos")
+
+#  # Facilitar para outros programas
+# print(f"Acesso: {acesso}")
