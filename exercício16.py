3ebf872e166b370fab03f6c65e36f0e231c4b7a2
Gabriel Antonio Andrighetto <107733745+Gajjd@users.noreply.github.com>
2025-04-05 12:34:31 -0400

    Update exercício16.py

--- exercício16.py
+++ exercício16.py
@@ -1,14 +1,24 @@
 nota1 = float(input("1° nota: "))
 nota2 = float(input("2° nota: "))
 nota3 = float(input("3° nota: "))
+
 media = (nota1 + nota2 + nota3) / 3
+
 aprovado = False
 recuperacao = False
-if media >=7:
+reprovado = False
+
+if media >= 7:
     aprovado = True
     print("Aprovado")
-elif media <7 and media >=5:
-    recuperacao = True 
+elif 7 > media >= 5:
+    recuperacao = True
     print("Recuperação")
 else:
+    reprovado = True
     print("Reprovado")
+
+#  #Essa parte seria mais para caso a escola quisesse ter a tomada de decisão automático
+# print(f"Aprovado: {aprovado}")
+# print(f"Recuperação: {recuperacao}")
+# print(f"Reprovado: {reprovado}")
